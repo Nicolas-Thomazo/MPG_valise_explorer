482bf25c8750e7353c26984505aec1e6ee420f9d
nicolas <nicolas.thomazo@outlook.com>
2026-02-01 16:27:14 +0100

    fix logging config

--- mpg_explorer/__init__.py
+++ mpg_explorer/__init__.py
@@ -1,4 +1,36 @@
-import logging
+from logging import basicConfig, getLogger
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+
+class LeagueConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # CREDENTIALS
+    MPG_USERNAME: str | None = Field(default=None, env="MPG_USERNAME")
+    PASSWORD: str | None = Field(default=None, env="MPG_PASSWORD")
+
+    # LEAGUE CONFIG
+    LEAGUE_ID: str | None = Field(default=None, env="LEAGUE_ID")
+    TEAM_NAME: str | None = Field(default=None, env="TEAM_NAME")
+    RESULTS_LINK: str | None = Field(default=None, env="RESULT_LINK")
+    SEASON_NUMBER: int | None = Field(default=None, env="SEASON_NUMBER")
+    DIVISION: int | None = Field(default=None, env="DIVISION")
+    NUMBER_PLAYERS: int | None = Field(default=None, env="NUMBER_PLAYERS")
+
+    # CONFIG
+    IS_DOCKER: bool = Field(default=True, env="IS_DOCKER")
+    MATCHWEEK: list | None = Field(default=None, env="MATCHWEEK")
+    AZURE_STORAGE_CONNECTION_STRING: str | None = Field(
+        default=None, env="AZURE_STORAGE_CONNECTION_STRING"
+    )
+
+
+LEAGUE_CONFIG = LeagueConfig()
+
+
+basicConfig(
+    level="INFO",
+    format="%(levelname)s - %(message)s",  # %(asctime)s
+)
+logger = getLogger("mpg-explorer")
--- mpg_explorer/utils/driver.py
+++ mpg_explorer/utils/driver.py
@@ -3,11 +3,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 from utils.selenium import get_url
 
@@ -42,7 +40,9 @@ class Driver:
             try:
                 self.driver.switch_to.frame(iframe)
                 button_XPATH = "//button[@class='sc-furwcr jhwOCG button button--filled button__acceptAll']"
-                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_XPATH))).click()
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, button_XPATH))
+                ).click()
                 accepted = True
                 logging.info("BINGO COOKIES ACCEPTED.")
                 break
@@ -64,7 +64,7 @@ class Driver:
         except:
             raise NameError("Couldn't accept cookies.")
 
-    def logging(self, user, password):
+    def login_mpg(self, user, password):
         """
         Logging user.
         1- Getting base URL
@@ -77,14 +77,23 @@ class Driver:
         :param password: User password
         """
         get_url(driver=self.driver, url=self.url)
-        self.driver.fullscreen_window()
+        # self.driver.fullscreen_window()
 
         self.accept_cookies()
 
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "login"))).send_keys(user)
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(password)
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[text()='Se connecter']"))
+        )
+        element.click()
 
-        buttons = self.driver.find_elements(By.TAG_NAME, "button")
-        for button in buttons:
-            if button.text == "Je me connecte":
-                button.click()
+        login_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "username"))
+        )
+        login_input.clear()
+        login_input.send_keys(user)
+
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "password"))
+        )
+        password_input.clear()
+        password_input.send_keys(password)
