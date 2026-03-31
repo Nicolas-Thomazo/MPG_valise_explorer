f155ae5c8713ee346c57eb5422f928840e74e9ee
nicolas <nicolas.thomazo@outlook.com>
2026-03-31 22:12:31 +0200

    temp

--- mpg_explorer/utils/driver.py
+++ mpg_explorer/utils/driver.py
@@ -89,7 +89,7 @@ class Driver:
             EC.element_to_be_clickable((By.XPATH, "//*[text()='Se connecter']"))
         )
         element.click()
-
+        logger.info(LEAGUE_CONFIG)
         path = Path("/app/data_kestra")
         filename = path / "error_screenshot.png"
         logger.info(f"THIS IS THE SNAPSHOT SAVE {filename}")
